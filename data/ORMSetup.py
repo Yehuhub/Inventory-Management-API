6bd8f2881d2eb6ab7aaa5376bd74737db38b17d2
Lara Duek <laradu@edu.hac.ac.il>
2025-06-15 18:39:37 +0300

    add orders, order_items, client, price tables

--- data/ORMSetup.py
+++ data/ORMSetup.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 from sqlalchemy import Column, Integer, String
 
 # Create an engine (SQLite for this example)
-engine = create_engine("postgresql+psycopg2://yehu:Aa123456@localhost:5432/test_db")
+engine = create_engine("postgresql+psycopg2://admin:Aa123456@localhost:5433/dbname")
 
 # Base class for models
 Base = declarative_base()
