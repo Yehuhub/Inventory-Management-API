90cbfd752130c24fed87d585b163b66db4c63974
Yehuhub <yhrcch@gmail.com>
2025-06-19 12:40:08 +0300

    added item stocks repository

---
+++ repository/item_stock_repository.py
@@ -0,0 +1,28 @@
+from repository.base_repository import BaseRepository
+from models import ItemStock
+from sqlalchemy import select
+from sqlalchemy.orm import selectinload
+
+class ItemStockRepository(BaseRepository):
+    def __init__(self, db):
+        super().__init__(db, ItemStock)
+
+    def get_item_by_stock_id(self, stock_id):
+        stmt = select(ItemStock).where(ItemStock.id == stock_id).options(selectinload(ItemStock.item))
+        item_stock = self.db.execute(stmt).scalars().first()
+        return item_stock.item if item_stock else None
+
+    def get_item_stock_by_item_id(self, item_id):
+        stmt = select(ItemStock).where(ItemStock.item_id == item_id)
+        return self.db.execute(stmt).scalars().all()
+    
+    def get_item_stock_by_branch_id(self, branch_id):
+        stmt = select(ItemStock).where(ItemStock.branch_id == branch_id)
+        return self.db.execute(stmt).scalars().all()
+    
+    def get_item_stock_by_branch_and_item_id(self, branch_id, item_id):
+        stmt = select(ItemStock).where(
+            ItemStock.branch_id == branch_id,
+            ItemStock.item_id == item_id
+        )
+        return self.db.execute(stmt).scalars().first()
