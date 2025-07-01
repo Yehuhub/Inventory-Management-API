15c67269e0abf8620f5aabc32208e6ac54af214d
Yehuhub <yhrcch@gmail.com>
2025-07-01 21:41:21 +0300

    added routes to categories to get all items under that requested category

--- data/ORMSetup.py
+++ data/ORMSetup.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-# Create an engine (SQLite for this example)
+# Create an engine (this string should actually be in a .env file)
 engine = create_engine("postgresql+psycopg2://admin:Aa123456@localhost:5433/dbname")
 
 # Base class for models
